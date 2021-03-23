c72b1c81fc4daba4b952fe58731827f1014cca12
liaoronghua <liaoronghua@elex-tech.com>
2021-03-23 16:49:51 +0800

    删除无用代码

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
-import AppStackNavigator from './AppNavigation';
 import YNMain from './src/main/YNMain'
 
 export default class App extends Component {
   render() {
     return (
-      // <AppStackNavigator/>
       <YNMain/>
     )
   }
--- index.js
+++ index.js
@@ -3,9 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import {createAppContainer} from 'react-navigation';
 import App from './App'
 import {name as appName} from './app.json';
 
-// const AppStackNavigatorContainer = createAppContainer(App);
 AppRegistry.registerComponent(appName, () => App);
