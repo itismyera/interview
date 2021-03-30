0819b6e7299703164448116e95561f7c10fc431c
liaoronghua <liaoronghua@elex-tech.com>
2021-03-30 14:20:30 +0800

    ios打包配置

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# interview
+npx react-native run-ios 
+
+npx react-native run-android
+
+npm install react-native-vector-icons 装插件
+
+cd ios & pod install
+
+npm run bundle-ios 打包ios时需要
+
+打包连接
+
+http://blog.poetries.top/2019/10/03/rn-ios-distribute/
+
+http://blog.poetries.top/2019/10/07/Ionic-ios-build/
+
+
+
--- ios/interview/AppDelegate.m
+++ ios/interview/AppDelegate.m
@@ -51,7 +51,8 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+//  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [[NSBundle mainBundle] URLForResource:@"bundle/index" withExtension:@"jsbundle"];
 #endif
 }
 
